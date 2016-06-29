f5d96b861e009ddc74e0177af054ca5ec440176e
cfrieden <cfrieden@redhat.com>
2016-06-29 01:46:47 -0400

    Getting the runners working again

--- app/lab-kie-test/src/test/java/com/rhc/lab/test/cucumber/RemoteRulesRunner.java
+++ app/lab-kie-test/src/test/java/com/rhc/lab/test/cucumber/RemoteRulesRunner.java
@@ -8,5 +8,5 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "classpath:features/", plugin = {"html:target/cucumber"}, tags = {
 		"~@Ignore", "~@not_implemented", "@Rules"}, glue = {"com.rhc.lab.test.cucumber.kie"})
-public class RunRemoteRulesTest {
+public class RemoteRulesRunner {
 }
